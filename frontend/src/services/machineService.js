c07d6ee864134bbfe5110334027ce4358a631f7c
Dulara Mihiran Jayasinghe <168359371+dularamihiran@users.noreply.github.com>
2025-10-03 00:32:02 +0530

    localhost api to deployed api

--- frontend/src/services/machineService.js
+++ frontend/src/services/machineService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://16.16.24.8:5000/api';
 
 // Machine API Service
 export const machineService = {
